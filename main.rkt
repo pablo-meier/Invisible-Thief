1134049967927d0864571d54a828726497dafbf9
Paul Meier <pablo.a.meier@gmail.com>
2013-01-08 15:51:35 -0800

    Remove dubugging println

--- main.rkt
+++ main.rkt
@@ -6,7 +6,6 @@
 (define (main)
   (let* ([args (current-command-line-arguments)]
          [filename (vector-ref args 0)])
-    (printf "filename is ~a and string? is ~a~n" filename (string? filename))
     (call-with-input-file filename
       (λ (file-input)
         (interp-program (parse #f file-input)))
